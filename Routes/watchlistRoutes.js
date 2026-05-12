a725e57155bdb6e830c8fa4208e884ae6d326865
Sameer <ahmad.sameer.5122@gmail.com>
2026-05-12 13:31:45 +0600

    Added validation using zod

--- Routes/watchlistRoutes.js
+++ Routes/watchlistRoutes.js
@@ -2,10 +2,12 @@ import express from 'express';
 import Router from 'express';
 import { addWatchListItem,deleteWatchListItem, updateWatchListItem } from '../Controllers/watchListController.js';
 import authMiddleWare from '../middleware/authMiddleware.js';
+import createWatchlistItemSchema from '../validators/createWatchListItemSchema.js';
+import validateRequest from '../middleware/validateRequests.js';
 
 const router =express.Router()
-router.use(authMiddleWare)
-router.post('/add',addWatchListItem)
+// router.use(authMiddleWare)
+router.post('/add',validateRequest(createWatchlistItemSchema),addWatchListItem)
 router.delete('/add/:id',deleteWatchListItem)
 router.put('/add/:id',updateWatchListItem)
 
--- validators/createWatchListItemSchema.js
+++ validators/createWatchListItemSchema.js
@@ -1,11 +1,14 @@
-import {z} from zod;
+import {z} from "zod";
 
 const createWatchlistItemSchema = z.object({
-    movieId: z.string().uui(),
-    status: z.enum(["TO_WATCH", "WATCHING", "WATCHED"],{error: ()=>{
-        message: "Status must be one of : TO_WATCH, WATCHING, WATCHED"
-    }}).optional(),
-    rating: z.coerce.number().float("Rating must be float").min(1, "Rating must be between 1 to 10").max(10,"Rating must be between 1 to 10").optional(),
+    movieId: z.string().uuid("Invalid Movie ID format"), // Standard UUID check
+    status: z.enum(["TO_WATCH", "WATCHING", "WATCHED"], {
+        errorMap: () => ({ message: "Status must be one of: TO_WATCH, WATCHING, WATCHED" })
+    }).optional(),
+    rating: z.coerce.number()
+        .min(1, "Rating must be between 1 to 10")
+        .max(10, "Rating must be between 1 to 10")
+        .optional(),
     review: z.string().optional(),
 })
 export default createWatchlistItemSchema;
